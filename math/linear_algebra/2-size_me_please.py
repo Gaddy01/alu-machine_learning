372ff9819a08a2a89e6892a020011cfcae546517
Gaddy01 <g.irakoze2@alustudent.com>
2026-02-02 05:31:12 -0800

    commit 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
+"""
+Module that provides a function to compute the shape of a matrix.
+"""
 def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix.
+    Args:
+        matrix (list): A list representing a matrix (possibly multi-dimensional)
+    Returns:
+        list: A list of integers representing the shape of the matrix
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
