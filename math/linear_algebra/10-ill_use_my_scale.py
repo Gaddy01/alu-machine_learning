7d5f11b467fcf3fbba09c64b4a6bbd7ba481ae94
Gaddiel Irakoze <g.irakoze2@alustudent.com>
2026-02-02 16:34:04 +0200

    Update 10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function to get the shape of a numpy ndarray.
+"""
+
+
+def np_shape(matrix):
+    """
+    Returns the shape of a numpy ndarray.
+    Args:
+        matrix (numpy.ndarray): The input array
+    Returns:
+        tuple: Shape of the array
+    """
+    return matrix.shape
