d3f1a9844e84dc2f4caf304a102c6b88e61d55f3
Gaddiel Irakoze <g.irakoze2@alustudent.com>
2026-02-02 16:48:26 +0200

    Update 14-saddle_up.py

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function to perform matrix multiplication using NumPy.
+"""
+
+import numpy as np
+
+def np_matmul(mat1, mat2):
+    """
+    Multiplies two numpy arrays using matrix multiplication.
+    Args:
+        mat1 (numpy.ndarray): First matrix
+        mat2 (numpy.ndarray): Second matrix
+    Returns:
+        numpy.ndarray: Result of mat1 @ mat2
+    """
+    return np.matmul(mat1, mat2)
