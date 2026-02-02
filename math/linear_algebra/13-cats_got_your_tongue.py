06447811ab04c88d6fc67fcf9e4d1540df9e376a
Gaddiel Irakoze <g.irakoze2@alustudent.com>
2026-02-02 16:50:25 +0200

    Update 13-cats_got_your_tongue.py

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function to concatenate two numpy matrices along a specified axis.
+"""
+
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    Concatenates two matrices along the given axis.
+    Args:
+        mat1 (array-like): First matrix
+        mat2 (array-like): Second matrix
+        axis (int): Axis along which to concatenate (default 0)
+    Returns:
+        numpy.ndarray: Concatenated matrix
+    """
+    return np.concatenate((mat1, mat2), axis=axis)
