58d3e8c75aa3ff2fe6e10887d9d5cb5cb9f83a9d
Gaddiel Irakoze <g.irakoze2@alustudent.com>
2026-02-02 16:28:25 +0200

    Update 5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -18,4 +18,3 @@ def add_matrices2D(mat1, mat2):
     if any(len(r1) != len(r2) for r1, r2 in zip(mat1, mat2)):
         return None
     return [[a + b for a, b in zip(r1, r2)] for r1, r2 in zip(mat1, mat2)]
-
