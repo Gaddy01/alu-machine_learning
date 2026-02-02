4357b5d1fab34799d3d491d3fbba0296ab88f55b
Gaddiel Irakoze <g.irakoze2@alustudent.com>
2026-02-02 16:25:56 +0200

    Update 5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -11,8 +11,11 @@ def add_matrices2D(mat1, mat2):
         mat1 (list of lists of int/float): First matrix
         mat2 (list of lists of int/float): Second matrix
     Returns:
-        list of lists: New matrix with element-wise sums, or None if shapes differ
+        list of lists: New matrix with element-wise sums
     """
-    if len(mat1) != len(mat2) or any(len(r1) != len(r2) for r1, r2 in zip(mat1, mat2)):
+    if len(mat1) != len(mat2):
+        return None
+    if any(len(r1) != len(r2) for r1, r2 in zip(mat1, mat2)):
         return None
     return [[a + b for a, b in zip(r1, r2)] for r1, r2 in zip(mat1, mat2)]
+
